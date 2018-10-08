ce130e062541246325f1da737d5ec761088ec358
malasiot <malasiot@iti.gr>
2018-10-08 14:22:19 +0300

    refactoring

--- src/certh_dataset.hpp
+++ src/certh_dataset.hpp
@@ -3,6 +3,26 @@
 
 #include "dataset.hpp"
 
+/*
+ *  For each object create a folder with the name of the object and an optional variant e.g. cup_v1/
+ *  On this folder put the following files.
+ *
+ *  box.txt (3D model extents: X Y Z)
+ *  w2m.txt (4x4 matrix from world to model coordinates)
+ *  model.obj ( 3D triangle model of the object )
+ *  merged.ply (a point cloud of the object e.g. obtained by merging several views or by model sampling)
+ *
+ *  pose_XXXXX.png: view pose (4x4)
+ *  rgb_XXXXX.png:  view rgb image
+ *  depth_XXXXX.png: view depth image
+ *  mask_XXXXX.png:  view mask (object vs background)
+ *
+ * where XXXXX is the view id ([0-9]+)
+ *
+ * Background images are those of the BG_Rooms dataset
+ * https://hci.iwr.uni-heidelberg.de/vislearn/wp-content/uploads/2018/03/readme_bg.pdf
+ *
+ * */
 
 struct CERTH_Dataset: public Dataset {
     CERTH_Dataset(): Dataset() {}
